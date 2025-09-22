ef2c33c5c5a57632fec5a3ce003019142c125a5e
SuzzyTech <adekunleademola2406@gmail.com>
2025-09-22 16:29:26 +0100

    Update autostatus.js

--- commands/autostatus.js
+++ commands/autostatus.js
@@ -6,8 +6,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '120363421299105029@newsletter',
+            newsletterName: 'SuzzyTech',
             serverMessageId: -1
         }
     }
