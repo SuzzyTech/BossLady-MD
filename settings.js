114c3645e70805f75001041ae85a65eb54cacafa
SuzzyTech <adekunleademola2406@gmail.com>
2025-09-23 05:05:17 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,16 +1,16 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'BossLady',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "BossLady",
+  botOwner: 'SuzzyTech', // Your name
+  ownerNumber: '2349010288808', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.1.9",
-  updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
+  updateZipUrl: "https://github.com/SuzzyTech/BossLady-MD/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
